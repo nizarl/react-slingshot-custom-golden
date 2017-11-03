9d4bcf10db487e5bcdb2f007c743e7bb855712e2
Nizar Lakhdhir <nizar.lakhdhir@chenmed.com>
2017-11-03 07:23:39 -0400

    removed underscore..using Object.entries

--- src/utils/path.service.js
+++ src/utils/path.service.js
@@ -1,5 +1,4 @@
 import config from '../project.properties';
-import _ from 'underscore';
 
 //The browser will always resolve window.location.hostname.  However, we supply localhost for Unit test purposes only.
 const hostname = (window && window.location && window.location.hostname) || "localhost";
@@ -7,15 +6,16 @@ const hostname = (window && window.location && window.location.hostname) || "loc
 let envKey;
 let domains = config.businessServiceBaseUrls.domains;
 
-_.each(domains, function(value, key) {
+for (let [key, value] of Object.entries(domains)) { 
     if(value[0]===hostname){
         envKey = key;
-    }
-});
+    } 
+  }
 
 if(!envKey){
     throw new Error("Business service URL not found. Check project.properties.json file for correct config values.");
 }
 
 let apiUrl = config.businessServiceBaseUrls[envKey].careProAPI;
+
 export const BASE_URL = apiUrl;
